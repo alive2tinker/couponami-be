0dac5ad211ca08b310b7ce0324d11dd3b2c8a5e8
Abdulmalik Alsufayran <alsufiran@gmail.com>
2023-01-12 15:29:36 +0300

    implemented auth routes [WIP]

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,14 +23,14 @@ class AuthController extends Controller
             'device_name' => 'required',
         ]);
 
-    $user = User::where('email', $request->email)->first();
+        $user = User::where('email', $request->email)->first();
 
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
 
-    return $user->createToken($request->device_name)->plainTextToken;
+        return $user->createToken($request->device_name)->plainTextToken;
     }
 }
