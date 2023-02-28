2f4ed70c771ee3f14e74dd08a00673e858b6872e
abdulmalik alsufayran <alsufiran@gmail.com>
2023-02-28 17:44:23 +0300

    privacy controller

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,3 +1,6 @@
 <x-guest-layout>
-{{ strip_tags($privacy->value)}}
+    <h1 class="text-2xl text-center py-7 font-bold">@lang('Privacy Policy')</h1>
+<div class="h-80 overflow-y-scroll">
+    {{ strip_tags($privacy->value)}}
+</div>
 </x-guest-layout>
